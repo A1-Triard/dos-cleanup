2409f513d907a14b3ee8d7be938b51206c99eedd
Warlock <internalmike@gmail.com>
2023-08-29 13:25:51 +0300

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+
+use core::sync::atomic::{AtomicBool, Ordering};
+
+struct CleanupGuard<'a>(&'a mut Option<Cleanup>);
+
+static CLEANUP_GUARD: AtomicBool = AtomicBool::new(false);
+
+static mut CLEANUP: Option<Cleanup> = None;
+
+impl<'a> CleanupGuard<'a> {
+    fn acquire() -> Self {
+        loop {
+            if CLEANUP_GUARD.compare_exchange_weak(false, true, Ordering::SeqCst, Ordering::Relaxed).is_ok() {
+                break;
+            }
+        }
+        CleanupGuard(unsafe { &mut CLEANUP })
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<'a> Drop for CleanupGuard<'a> {
+    fn drop(&mut self) {
+        CLEANUP_GUARD.store(false, Ordering::SeqCst);
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+struct Cleanup {
+}
+
+pub fn init() {
+    let cleanup = CleanupGuard::acquire();
+    if cleanup.0.is_none() {
+        cleanup.0.replace(Cleanup { });
     }
 }
